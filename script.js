07744c0c65f69a83c5a5ca8c83061c443dd6a6d8
Corey Deeves <coreydeeves2@gmail.com>
2023-04-04 20:52:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,37 +20,3 @@ toggle.addEventListener('click', () => {
   bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`))
   bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
 })
-  
-//   var bowling = document.getElementById('bowling');
-//   var project = document.getElementById('project');
-//   var laundro = document.getElementById('laundro');
-//   var zombie = document.getElementById('zombie');
-  
-//   //fade out
-//   function FadeOutOnScroll(element) {
-//      if (!element) {
-//          return;
-//      }
-  
-//      var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-//      var elementHeight = element.offsetHeight;
-//      var scrollTop = document.documentElement.scrollTop;
-//      var opacity = 1;
-  
-//      if (scrollTop > distanceToTop) {
-//          opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-//      }
-  
-//      if (opacity >= 0) {
-//          element.style.opacity = opacity;
-//      }
-//   }
-  
-//   function scrollHandler() {
-//      FadeOutOnScroll(bowling);
-//      FadeOutOnScroll(project);
-//      FadeOutOnScroll(laundro);
-//      FadeOutOnScroll(zombie);
-//   }
-  
-//   window.addEventListener('scroll', scrollHandler)
